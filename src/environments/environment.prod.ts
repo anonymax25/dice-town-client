d6c61f215f3a2e0dee225b429d093cfc3dfa9c2f
maxime.dharboulle <maxime.dharboulle@sequodia.com>
2021-03-28 12:44:41 +0200

    edit prod ports

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   title: 'Dice Town Online',
-  apiUrl: 'http://ec2-34-240-42-153.eu-west-1.compute.amazonaws.com:80/api/v1/',
-  socketUrl: 'http://ec2-34-240-42-153.eu-west-1.compute.amazonaws.com:3001/api/v1/',
+  apiUrl: 'http://ec2-34-254-173-6.eu-west-1.compute.amazonaws.com/api/v1/',
+  socketUrl: 'http://ec2-34-254-173-6.eu-west-1.compute.amazonaws.com:3001/api/v1/',
   socketPath: '/websockets',
   alertSocketNamespace: 'alerts',
   chatSocketNamespace: 'chat',
