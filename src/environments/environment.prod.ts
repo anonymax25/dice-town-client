1c200ac6c5404850ff90371df37e12a5d20dc45e
maxime.dharboulle <maxime.dharboulle@sequodia.com>
2021-03-31 21:01:22 +0200

    edit conf

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   title: 'Dice Town Online',
   apiUrl: 'http://88.121.191.251:9090/',
-  socketUrl: 'http://88.121.191.251:3000/',
+  socketUrl: 'http://88.121.191.251:9090/',
   socketPath: '/websockets',
   alertSocketNamespace: 'alerts',
   chatSocketNamespace: 'chat',
